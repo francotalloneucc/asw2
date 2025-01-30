1d94ff06548792c55fecae990d841f3f513cc185
francotalloneucc <2109774@ucc.edu.ar>
2025-01-30 04:12:11 -0300

    Primer commit en el nuevo repositorio

---
+++ user-api/routes/userRoutes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"user-reservation-api/controllers"
+	"user-reservation-api/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+// SetupUserRoutes define las rutas relacionadas con usuarios
+func SetupUserRoutes(router *gin.Engine) {
+	userGroup := router.Group("/users")
+	{
+		userGroup.POST("/register", controllers.SignUp)                               // Registro de usuario
+		userGroup.POST("/login", controllers.Login)                                   // Inicio de sesión
+		userGroup.GET("/validate", middleware.RequireAuth, controllers.Validate)      // Validar sesión
+		userGroup.GET("/current", middleware.RequireAuth, controllers.GetCurrentUser) // Obtener usuario actual
+		userGroup.POST("/logout", controllers.Logout)                                 // Cerrar sesión
+	}
+}
