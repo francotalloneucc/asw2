41514b862c155c7c59d228e2f713325792ffdefb
LeonardoMendezR <2108727@ucc.edu.ar>
2025-02-04 09:31:34 -0300

    ahora sirve la busqueda parcial pero con *

--- search-api/solr/solr.go
+++ search-api/solr/solr.go
@@ -18,42 +18,37 @@ type Hotel struct {
 
 // Enviar el hotel a Solr
 func SendToSolr(message []byte) error {
-	// Deserializar el mensaje recibido
 	var hotel Hotel
 	err := json.Unmarshal(message, &hotel)
 	if err != nil {
-		return fmt.Errorf("Error unmarshaling message: %v", err)
+		return fmt.Errorf("error unmarshaling message: %v", err)
 	}
 
-	// Convertir el hotel a JSON para Solr
 	jsonData, err := json.Marshal(map[string]interface{}{
 		"add": map[string]interface{}{
 			"doc": hotel,
 		},
 	})
 	if err != nil {
-		return fmt.Errorf("Error marshaling data to Solr: %v", err)
+		return fmt.Errorf("error marshaling data to Solr: %v", err)
 	}
 
-	// Enviar el documento a Solr
 	solrURL := "http://localhost:8983/solr/hotel_core/update?commit=true"
 	req, err := http.NewRequest("POST", solrURL, bytes.NewBuffer(jsonData))
 	if err != nil {
-		return fmt.Errorf("Error creating request: %v", err)
+		return fmt.Errorf("error creating request: %v", err)
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("Error sending request to Solr: %v", err)
+		return fmt.Errorf("error sending request to Solr: %v", err)
 	}
 	defer resp.Body.Close()
 
-	// Verificar la respuesta de Solr
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("Solr returned an error: %v", resp.Status)
+		return fmt.Errorf("solr returned an error: %v", resp.Status)
 	}
 
 	log.Println("Document successfully indexed in Solr.")
@@ -61,18 +56,20 @@ func SendToSolr(message []byte) error {
 }
 
 // SearchHotels busca hoteles en Solr según una consulta de búsqueda
-func SearchHotels(query string) (map[string]interface{}, error) {
+func SearchHotels(city string) (map[string]interface{}, error) {
+	// Construcción de la consulta para búsqueda exacta o por prefijo
+	query := fmt.Sprintf("city:%s*", city)
 	solrURL := fmt.Sprintf("http://localhost:8983/solr/hotel_core/select?q=%s&wt=json", query)
 
 	resp, err := http.Get(solrURL)
 	if err != nil {
-		return nil, fmt.Errorf("Error al hacer la solicitud a Solr: %v", err)
+		return nil, fmt.Errorf("error al hacer la solicitud a Solr: %v", err)
 	}
 	defer resp.Body.Close()
 
 	var result map[string]interface{}
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		return nil, fmt.Errorf("Error al decodificar la respuesta de Solr: %v", err)
+		return nil, fmt.Errorf("error al decodificar la respuesta de Solr: %v", err)
 	}
 
 	return result, nil
